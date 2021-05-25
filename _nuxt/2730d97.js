19e37d6ddfecd2389f3aa0062d8974b495ee5257
BartKnucle <BartKnucle@users.noreply.github.com>
2021-05-25 10:24:32 +0000

    deploy: FunkySheepStudio/Website@f77f020d780fd3c782088321e5c8d21daf14561e

--- _nuxt/2730d97.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{727:function(n,e,t){"use strict";t.r(e);var c={},l=t(44),component=Object(l.a)(c,(function(){var n=this,e=n.$createElement;return(n._self._c||e)("section",[n._v("\n  donate\n")])}),[],!1,null,null,null);e.default=component.exports}}]);
